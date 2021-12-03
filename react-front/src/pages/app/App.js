dcb25cb66a9a881be77efb91742c67a6f42eacb3
emelygarcia <garciam.emm@gmail.com>
2021-12-03 07:56:49 -0600

    Login layout

--- react-front/src/pages/app/App.js
+++ react-front/src/pages/app/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import LoginComponent from '../login/login';
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
 
 function App() {
   return (
+    <ScopedCssBaseline enableColorScheme >
+    {/* The rest of your application */}
     <LoginComponent/>
+  </ScopedCssBaseline>
   );
 }
 
--- react-front/src/pages/login/login.js
+++ react-front/src/pages/login/login.js
@@ -1,7 +1,38 @@
-function LoginComponent() {
-    return ( <div className="container">
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import InputAdornment from "@mui/material/InputAdornment";
+import { AccountCircle } from "@mui/icons-material";
 
-    </div> );
+function LoginComponent() {
+  return (
+    <div className="container-login container-100 center-flex">
+      <div className="card card-login">
+        <h1>CQL-Teacher</h1>
+        <p>Learn and practice SQL with us!</p>
+        <form className="form-vertical-group">
+        <TextField
+          margin="normal"
+          label="Email"
+          id="outlined-start-adornment"
+          sx={{ m: 1, width: '25ch' }}
+          InputProps={{
+            startAdornment: <InputAdornment position="start"> <AccountCircle /></InputAdornment>,
+          }}
+        />
+        <TextField
+          margin="normal"
+          label="Password"
+          id="outlined-start-adornment"
+          sx={{ m: 1, width: '25ch' }}
+          InputProps={{
+            startAdornment: <InputAdornment position="start"> <AccountCircle /></InputAdornment>,
+          }}
+        />
+          <Button variant="contained">Log In</Button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default LoginComponent;
